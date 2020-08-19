14990b544c54fca0ceacb72fafa1de318c40242c
Mariusz <mar.lewandowski@gmail.com>
2020-08-19 13:51:01 +0200

    filtering works

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // base
 
 const baseOfTask = [
-  { id: 1, text: 'Pierwsze zadanie', complete: false },
-  { id: 2, text: 'Drugie zadanie', complete: false }
+  { id: 1, text: 'Pierwsze zadanie abcde', complete: false },
+  { id: 2, text: 'Drugie zadanie abcd', complete: false },
+  { id: 3, text: 'Trzecie zadanie abc', complete: false },
+  { id: 4, text: 'Czwarte zadanie ab', complete: false },
+  { id: 5, text: 'Piąte zadanie a', complete: false },
 ];
 
 // print li`s
@@ -35,6 +38,25 @@ const filterTasks = (e) => {
 }
 
 filter.addEventListener('input', filterTasks)
+
+//add tasks
+
+const formAdd = document.querySelector('form.addTask');
+const addTask = e => {
+  e.preventDefault()
+  const newTask = {
+    id: baseOfTask.length,
+    text: e.target[1].value,
+    complete: false
+  };
+  e.target[1].value = "";
+  baseOfTask.push(newTask);
+  const filterValue = document.querySelector('input#filter').value;
+  printTasks(filterValue);
+
+}
+formAdd.addEventListener('submit', addTask);
+
 
 //First Print on site
 

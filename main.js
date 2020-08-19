fef9d62a96e10b63f6bf221af7ec903cf18fd638
Mariusz <mar.lewandowski@gmail.com>
2020-08-19 13:26:02 +0200

    next todolist steps

--- main.js
+++ main.js
@@ -1,86 +1,41 @@
-// baza
+// base
 
-const taskBase = [
-  { id: 0, task: "wstać i się uczyć" },
-  { id: 1, task: "powtórzyć coś dla utrwalenia" },
-  { id: 2, task: " coś dla utrwalenia" },
-  { id: 3, task: " utrwalenia" },
+const baseOfTask = [
+  { id: 1, text: 'Pierwsze zadanie', complete: false },
+  { id: 2, text: 'Drugie zadanie', complete: false }
 ];
-//usuwanie zadań
-const deleteTask = e => {
-  e.preventDefault()
-  taskBase.splice(e.target.className, 1)
-  let idd = 0;
-  const newBase = [];
-  taskBase.forEach(task => {
-    const newTask = { id: idd, task: task.task }
-    newBase.push(newTask)
-    idd++;
-  })
-  taskBase.splice(0)
-  newBase.forEach(task => {
-    taskBase.push(task)
-  });
-  printTasks()
-}
-//edytowanie zadań
-const editValue = e => {
-  taskBase[e.target.className].task = e.target.value;
-  e.path[0].remove();
-  printTasks()
-}
-const editTask = e => {
-  e.preventDefault()
-  const paragraphForEditTask = document.createElement('p')
-  const inputForEditTask = document.createElement('input')
-  inputForEditTask.classList.add(e.target.className);
-  inputForEditTask.value = taskBase[e.target.className].task;
-  paragraphForEditTask.appendChild(inputForEditTask);
-  document.querySelector(`li:nth-child(${e.target.className * 1 + 1})`).insertBefore(paragraphForEditTask, document.querySelector(`li:nth-child(${e.target.className * 1 + 1})>button`))
-  document.querySelector(`li:nth-child(${e.target.className * 1 + 1}) p`).remove()
-  inputForEditTask.addEventListener('change', editValue)
-  // taskBase[e.target.className].task =
-}
-//wyświetlenie zadań
-const ul = document.querySelector('ul.taskList')
-const printTasks = () => {
-  ul.textContent = "";
-  taskBase.forEach(task => {
+
+// print li`s
+
+const printTasks = (filterTasks) => {
+  const ul = document.querySelector('ul');
+  ul.textContent = '';
+  let baseToPrint;
+  if (filterTasks === undefined) {
+    baseToPrint = baseOfTask;
+  } else {
+
+    baseToPrint = baseOfTask.filter(task => task.text.includes(filterTasks))
+  }
+
+  baseToPrint.map(task => {
     const li = document.createElement('li');
-    const p = document.createElement('p');
-    const btnDel = document.createElement('button');
-    const btnEdit = document.createElement('button');
-    p.textContent = task.task;
-    btnDel.textContent = "Usuń";
-    btnEdit.textContent = "Edytuj";
-    li.classList.add(task.id)
-    p.classList.add(task.id)
-    btnDel.classList.add(task.id)
-    btnEdit.classList.add(task.id)
-    btnDel.addEventListener('click', deleteTask)
-    btnEdit.addEventListener('click', editTask)
-    li.appendChild(p)
-    li.appendChild(btnDel)
-    li.appendChild(btnEdit)
+    li.textContent = task.text;
     ul.appendChild(li)
   })
 }
 
-//dodawanie zadań
+//filtering thing
+const filter = document.querySelector('input#filter');
 
-const addTaskForm = document.querySelector('form.addTask')
-
-const addTask = e => {
-  e.preventDefault();
-  const newTask = {
-    id: taskBase.length,
-    task: e.target[0].value,
-  }
-  taskBase.push(newTask)
-  e.target[0].value = "";
-  printTasks()
+const filterTasks = (e) => {
+  e.preventDefault()
+  const filterValue = e.target.value;
+  printTasks(filterValue)
 }
-addTaskForm.addEventListener('submit', addTask)
-//tylko wydrukowanie zadań
-printTasks()
 
+filter.addEventListener('input', filterTasks)
+
+//First Print on site
+
+printTasks()

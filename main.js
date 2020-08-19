4fa82173cdef89393521d138022ee0fe3c968158
Mariusz <mar.lewandowski@gmail.com>
2020-08-19 17:41:21 +0200

    add delete btn

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
 // base
 
 const baseOfTask = [
-  { id: 1, text: 'Pierwsze zadanie abcde', complete: false },
-  { id: 2, text: 'Drugie zadanie abcd', complete: false },
+  { id: 1, text: 'Piąte zadanie a', complete: false },
+  { id: 2, text: 'Czwarte zadanie ab', complete: false },
   { id: 3, text: 'Trzecie zadanie abc', complete: false },
-  { id: 4, text: 'Czwarte zadanie ab', complete: false },
-  { id: 5, text: 'Piąte zadanie a', complete: false },
+  { id: 4, text: 'Drugie zadanie abcd', complete: false },
+  { id: 5, text: 'Pierwsze zadanie abcde', complete: false },
 ];
+
+//Deleting Tasks
+
+const removeTask = e => {
+  const liToDelete = e.target.className;
+
+  let id = 1;
+  const newBaseOfTask = [...baseOfTask]
+  // console.log("1", baseOfTask);
+  // console.log("1", newBaseOfTask);
+  baseOfTask.length = 0;
+  // console.log("2", baseOfTask);
+  // console.log("2", newBaseOfTask);
+  newBaseOfTask.map(task => {
+    if (`task_${task.id}` !== liToDelete) {
+      console.log(task.id, liToDelete);
+      baseOfTask.push({
+        id,
+        text: task.text,
+        complete: task.complete
+      })
+      id++;
+    };
+    console.log(id);
+  });
+  console.log("ok3", baseOfTask);
+  console.log("ok3", newBaseOfTask);
+  const filterValue = document.querySelector('input#filter').value;
+  printTasks(filterValue);
+}
 
 // print li`s
 
@@ -23,7 +53,13 @@ const printTasks = (filterTasks) => {
 
   baseToPrint.map(task => {
     const li = document.createElement('li');
+    const btn = document.createElement('button');
     li.textContent = task.text;
+    btn.textContent = "Usuń";
+    li.classList.add(`task_${task.id}`);
+    btn.classList.add(`task_${task.id}`);
+    btn.addEventListener('click', removeTask)
+    li.appendChild(btn)
     ul.appendChild(li)
   })
 }
